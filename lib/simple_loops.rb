3d5bfd5310c7f5996f1c5a5162fbf227c4e7ad33
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-18 07:13:43 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -18,3 +18,12 @@ def loop_message_n_times(string, number)
     counter += 1
   end
 end
+
+def output_array(array)
+  counter = 0 
+
+  while counter < array do
+    puts array
+    counter += 1
+  end
+end
