04b1ee52c281d403cb4b201775c2fd4b3524fdea
Hugo Delgado <hugo.delgado1@outlook.com>
2020-08-18 07:05:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+
+def loop_message_five_times (array)
+  counter = 0
+
+  while counter < 5 do
+    puts array
+    counter += 1
+  end
+end
+
+def loop_message_n_times (array, number)
+  counter = 0
+
+  while counter < number do
+    puts array
+    counter += 1
+  end
+end
